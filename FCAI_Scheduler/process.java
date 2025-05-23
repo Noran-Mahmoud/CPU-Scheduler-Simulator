aa8c3b992badf208af3bf11c30fb5fb472cee8aa
Noran Mahmoud <noran.m.ismail05@gmail.com>
2025-05-23 18:19:09 +0300

    added gui to process.java

--- FCAI_Scheduler/process.java
+++ FCAI_Scheduler/process.java
@@ -1,28 +1,32 @@
+import java.awt.Color;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.ArrayList;
 
 public class process extends Thread {
     private int id;
     private String name;
-    private String color;
+    private Color color;
     private int initialBurstTime;
     private int burstTime;
     private int arrivalTime;
     private int priority;
     private int quantum;
     private int FCAIFactor;
-    // private int contextSwitch;
     private double variables[];
     private Object monitor;//lock
     private Ready_Queue readyQueue; 
     private Instant startInst;
     private Instant finishInst;
+    private Instant pause;
     private int turnaroundTime;
     private int waitingTime;
+    private ArrayList<ExecutionSegment> timeLine;
+    // private int contextSwitch;
 
     public process(){}
 
-    public process(int id, String name, String color,int burstTime,int arrivalTime,int priority,int quantum,
+    public process(int id, String name, Color color,int burstTime,int arrivalTime,int priority,int quantum,
             double v[], Object monitor, Ready_Queue r){
         this.id = id;
         this.name = name;
@@ -33,17 +37,20 @@ public class process extends Thread {
         this.quantum = quantum;
         this.priority = priority;
         this.monitor = monitor;
-        // this.contextSwitch = contextSwitch;
         this.variables = v;
         this.readyQueue = r;
+        pause = null;
+        timeLine = new ArrayList<ExecutionSegment>();
+        // this.contextSwitch = contextSwitch;
         FCAIFactor = (10-priority) + (int) Math.ceil(arrivalTime/variables[0]) + (int) Math.ceil(burstTime/variables[1]);
-        System.out.println("P" + id + " factor is " + this.FCAIFactor);
+        System.out.println(name + " FCAI Factor is " + this.FCAIFactor);
     }
 
     @Override
     public void run() {
         try {
             Thread.sleep(arrivalTime*1000);
+            addSegment(arrivalTime,  Color.WHITE);
             startInst = Instant.now();
             // System.out.println("P" + id + " entered Ready Queue!");
             readyQueue.enterReadyQueue(this);
@@ -58,21 +65,31 @@ public class process extends Thread {
         try{
             while(burstTime > 0){
                     
+                if(burstTime == initialBurstTime){
+                    Instant now = Instant.now();
+                    addSegment((int) Duration.between(startInst, now).toSeconds(), Color.WHITE);
+                }
+                if(pause != null){
+                    Instant now = Instant.now();
+                    addSegment((int) Duration.between(pause, now).toSeconds(), Color.WHITE);
+                }
                 // System.out.println("P" + id + " entered CPU!");
 
                 int preemptiveTime =(int) Math.ceil(0.4*quantum);
                 Thread.sleep(preemptiveTime*1000);
                 burstTime  -= preemptiveTime;
                 int remainingQ = quantum - preemptiveTime;
+                addSegment(preemptiveTime, color);
 
                 while(remainingQ >0 && burstTime >0){
                     if (readyQueue.hasProcess() && readyQueue.getLessFactor() < this.FCAIFactor) {
                         FCAIFactor = (10-priority) + (int) Math.ceil(arrivalTime/variables[0]) + (int) Math.ceil(burstTime/variables[1]);
                         quantum += remainingQ;
+                        pause = Instant.now();
                         // Thread.sleep(contextSwitch * 1000);
 
-                        System.out.println("P" + id + " called notifyNextlessFactor");
-                        System.out.println("P" + id + " Remaining Burst Time:" + burstTime + " ,Quantum:" + (quantum-remainingQ) + "->" + quantum + " ,FCAI Factor:" + FCAIFactor);
+                        System.out.println(name + " called notify Next less Factor");
+                        System.out.println(name + " Remaining Burst Time:" + burstTime + " ,Quantum:" + (quantum-remainingQ) + "->" + quantum + " ,FCAI Factor:" + FCAIFactor);
                         readyQueue.notifyNextlessFactor(this);
                         return;
                     }
@@ -80,24 +97,24 @@ public class process extends Thread {
                     Thread.sleep( 1000);
                     remainingQ--;
                     burstTime--;
+                    addSegment(1, color);
                 }
               
                 if(burstTime <= 0){
-                    FCAIFactor =0;
-                    quantum = 0;
-                    System.out.println("P" + id + " Completed!");
+                    System.out.println(name + " Completed!");
                     finishInst = Instant.now();
                     turnaroundTime = (int) Duration.between(startInst, finishInst).toSeconds();
-                    System.out.print("Turnaround Time is:" + turnaroundTime);
+                    System.out.print("Turnaround Time:" + turnaroundTime);
                     waitingTime = turnaroundTime - initialBurstTime;
-                    System.out.println(" and Waiting Time:" + waitingTime);
+                    System.out.println(" ,Waiting Time:" + waitingTime);
                     readyQueue.leaveReadyQueue(this);
                 }
                 else if(remainingQ == 0){
                     quantum += 2;
                     FCAIFactor = (10-priority) + (int) Math.ceil(arrivalTime/variables[0]) + (int) Math.ceil(burstTime/variables[1]);
-                    System.out.println("P" + id + " called notifyNextEntered");
-                    System.out.println("P" + id + " Remaining Burst Time:" + burstTime + " ,Quantum:" + (quantum-2) + "->" + quantum + " ,FCAI Factor:" + FCAIFactor);
+                    System.out.println(name + " called notify Next Entered");
+                    System.out.println(name + " Remaining Burst Time:" + burstTime + " ,Quantum:" + (quantum-2) + "->" + quantum + " ,FCAI Factor:" + FCAIFactor);
+                    pause = Instant.now();
                     readyQueue.notifyNextEntered(this);
                 }
             }
@@ -110,12 +127,28 @@ public class process extends Thread {
         return id;
     }
 
+    public String getname(){
+        return name;
+    }
+
+    public Color getColor(){
+        return color;
+    }
+
     public int getArrivalTime(){
         return arrivalTime;
     }
 
     public int getBurstTime(){
         return burstTime;
+    }
+
+    public int getinitBurstTime(){
+        return initialBurstTime;
+    }
+
+    public int getpriority(){
+        return priority;
     }
 
     public int getFCAIFactor(){
@@ -132,5 +165,21 @@ public class process extends Thread {
 
     public int getWaitingTime(){
         return waitingTime;
+    }
+
+    public ArrayList<ExecutionSegment> getTimeLine(){
+        return timeLine;
+    }
+    private void addSegment(int duration, Color color){
+        timeLine.add(new ExecutionSegment(duration, color));
+    }
+}
+
+class ExecutionSegment {
+    int duration;
+    Color color;
+    public ExecutionSegment(int duration, Color color) {
+        this.duration = duration;
+        this.color = color;
     }
 }
